72b856d0c409b721601b4ee5d6669685a021b905
shivani9102 <shivani09d@gmail.com>
2024-07-03 15:41:56 +0530

    new branch created, and navebar and about us components saperated

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import About from '../../components/About/About.jsx'
+import Navbar from '../../components/Header/Navbar.jsx'
 import Header from '../../components/Header/Header.jsx'
 
 const HomePage = () => {
   return (
     <>
     <Header/>
+    <Navbar/>
     <About/>
     </>
     
   )
 }
 
-export default HomePage
+export default HomePage;
