d3febb0a17d3bafcbfe3cf1d7bc9c5efabed5950
kamalakar <kamalakar579@gmail.com>
2024-07-04 07:32:40 +0530

    added bootstrap js file in index.js

---
+++ client/src/components/WhyChooseSailorsWave/WhyChooseSailorsWave.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import WhyChoose1 from "../../assets/WhyChoose-1.jpeg";
+import WhyChoose2 from "../../assets/WhyChoose-2.jpeg";
+import WhyChoose3 from "../../assets/WhyChoose-3.jpeg";
+import "./WhyChooseSailorsWave.css";
+
+const WhyChooseSailorsWave = () => {
+    return (
+        <>
+            <div class="jumbotron jumbotron-fluid card-flow">
+                <h1>Why choose sailorswave</h1>
+                <div class="container row ">
+                    <div class="card" style={{ width: "25rem" }}>
+                        <img class="card-img-top" src={WhyChoose1} alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Expertise And Experience</b></h5>
+                            <p class="card-text">Our Team Comprises Seasoned Professionals With Extensive Experience in The Maritime industry.  We Bring Deep Insights And Practical Knowledge To Every Project We Undertake.</p>
+                        </div>
+                    </div>
+                    <div class="card" style={{ width: "25rem" }}>
+                        <img class="card-img-top" src={WhyChoose2} alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Customized Solutions</b></h5>
+                            <p class="card-text">We Understand That Every Client Has Unique Needs. Our Services Are Tailored To Meet The Specific Requirements Of Each Client, Ensuring Maximum Satisfaction And Efficiency.</p>
+                        </div>
+                    </div>
+                    <div class="card" style={{ width: "25rem" }}>
+                        <img class="card-img-top" src={WhyChoose3} alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title"><b>Commitment To Quality</b></h5>
+                            <p class="card-text">Quality Is At The Core Of Everything We Do. We Adhere To the Highest Standards In All Our Services, Ensuring Reliability And Excellence.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WhyChooseSailorsWave
--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from '../../components/About/About.jsx'
 import Header from '../../components/Header/Header.jsx'
+import WhyChooseSailorsWave from '../../components/WhyChooseSailorsWave/WhyChooseSailorsWave.jsx'
 
 const HomePage = () => {
   return (
     <>
     <Header/>
     <About/>
+    <WhyChooseSailorsWave/>
     </>
     
   )
