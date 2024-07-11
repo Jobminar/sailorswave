e33ae1ffcff428ba555dbcc81b3db927856da7da
kamalakar778 <kamalakar579@gmail.com>
2024-07-11 16:03:07 +0530

    created new ApplicationPage

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from './pages/HomePage/Home';
+import ApplicationPage from './pages/ApplicationPage/ApplicationPage.jsx';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<HomePage/> }/>
+        <Route path='/ApplicationPage' element={<ApplicationPage/> }/>
         <Route />
       </Routes>
     </Router>
