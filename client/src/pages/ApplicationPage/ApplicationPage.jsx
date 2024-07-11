db714baf09db620b7899916fdedea194a5404dd4
akshaya <aeleakshaya@gmial.com>
2024-07-11 18:13:05 +0530

    m

--- client/src/pages/ApplicationPage/ApplicationPage.jsx
+++ client/src/pages/ApplicationPage/ApplicationPage.jsx
@@ -23,32 +23,65 @@ const ApplicationPage = () => {
           </div>
         </div>
       </div>
-
-      <div className="form-div col">
-        <br />
-        <br />
-        <div className="text-center">
-          <h2>APPLICATION FORM FOR MERCHANT NAVY</h2>
-          <h2>APPLICATION FOR ADMISSION IN MARINE TRAINING</h2>
-        </div>
-        <br />
-        <br />
-        <form className="form-total-1">
+     
+      <div className="form-div">
+      <br />
+      <br />
+      <div className="text-center">
+        <h2>APPLICATION FORM FOR MERCHANT NAVY</h2>
+        <h2>APPLICATION FOR ADMISSION IN MARINE TRAINING</h2>
+      </div>
+      <br />
+      <br />
+        <form className="form-total">
           <p className="personal-details">PERSONAL DETAILS</p>
           <label className="candidate-label">Candidate Name</label>
           <br />
           <input type="text" />
-          {/* Akshaya _________________________________________ */}
 
-          <div>
-            <label></label>
-            <input />
+          {/* akshaya _________________________________________ */}
+
+          <div className="address-details">
+          <h3>ADDRESS OF CANDIDATES</h3>
+          <div className="form-group">
+            <label>House number</label>
+            <input type="text" placeholder="Input Text" />
           </div>
-          {/* Shivani _________________________________________ */}
+          <div className="form-group-1">
+            <label>Post office*</label>
+            <input type="text" placeholder="Input Text" />
+          </div>
+          <div className="form-group">
+            <label>Police station*</label>
+            <input type="text" placeholder="Input Text" />
+          </div>
+          <div className="form-group-1">
+            <label>District*</label>
+            <input type="text" placeholder="Input Text" />
+          </div>
+          <div className="form-group">
+            <label>Select city</label>
+            <select>
+              <option>Select city</option>
+            </select>
+          </div>
+          <div className="form-group-1">
+            <label>Select state</label>
+            <select>
+              <option>Select city</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Pincode</label>
+            <input type="text" placeholder="Input field" />
+          </div>
+        </div>
 
+
+          {/* shivani _________________________________________ */}
           <div>
             <label></label>
-            <input />
+            <input/>
           </div>
         </form>
       </div>
